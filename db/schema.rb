d9c92e772253d5c3f60d475e45e42c3a6b803e29
Jasmin Kang <jasminkang@Jasmins-MacBook-Pro.local>
2022-07-06 15:39:09 +1000

    create listings form

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_07_06_013755) do
+ActiveRecord::Schema.define(version: 2022_07_06_014702) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -52,6 +52,19 @@ ActiveRecord::Schema.define(version: 2022_07_06_013755) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "listings", force: :cascade do |t|
+    t.string "title"
+    t.integer "condition"
+    t.integer "price"
+    t.boolean "sold", default: false
+    t.bigint "user_id", null: false
+    t.bigint "category_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["category_id"], name: "index_listings_on_category_id"
+    t.index ["user_id"], name: "index_listings_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -66,4 +79,6 @@ ActiveRecord::Schema.define(version: 2022_07_06_013755) do
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "listings", "categories"
+  add_foreign_key "listings", "users"
 end
