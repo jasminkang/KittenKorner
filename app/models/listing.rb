545505d345eb3b890b26a24cd7bec68787eea26b
Jasmin Kang <jasminkang@Jasmins-MacBook-Pro.local>
2022-07-08 14:25:48 +1000

    updated listings form

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -3,7 +3,7 @@ class Listing < ApplicationRecord
   belongs_to :category
   has_rich_text :description
 
-  enum Condition: {
+  enum condition: {
      well_loved: 1,
      superficial_defects: 2,
      used_once: 3,
