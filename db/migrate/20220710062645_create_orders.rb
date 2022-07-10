407605627576439e0c37c7805ef1a562e195d810
Jasmin Kang <jasminkang@Jasmins-MacBook-Pro.local>
2022-07-10 16:37:53 +1000

    orders associations setup

--- db/migrate/20220710062645_create_orders.rb
+++ db/migrate/20220710062645_create_orders.rb
@@ -1,7 +1,7 @@
 class CreateOrders < ActiveRecord::Migration[6.1]
   def change
     create_table :orders do |t|
-      t.reference :listing
+      t.references :listing, null: false, foreign_key: true
       t.references :buyer, null: false, foreign_key: { to_table: :users}
       t.references :seller, null: false, foreign_key: { to_table: :users}
 
