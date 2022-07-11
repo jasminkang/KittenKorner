4805efe343816dca8e1fd11ec0009256b11295b1
Jasmin Kang <jasminkang@Jasmins-MacBook-Pro.local>
2022-07-11 14:33:36 +1000

    update views for no current user

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,4 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   post "listings/:id/order", to: "listings#place_order", as: "place_order"
 
-  get 'pages/success', to: 'pages#success', as: "order_success"
-
 end
